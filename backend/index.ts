09e047f230a079e90a00e4d029e89d199049820e
Shenghan Chen <zzdjk6@gmail.com>
2022-12-11 16:24:02 +1300

    update v2 checkbox example

---
+++ backend/index.ts
@@ -0,0 +1,116 @@
+import express from "express";
+import _ from "lodash";
+import axios from "axios";
+const app = express();
+const port = 9876;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const verifyWithSecret = (secret: string) => async (req: any, res: any) => {
+  console.log({ body: req.body });
+
+  const token = _.get(req.body, "token");
+  const response = await axios.post("https://www.google.com/recaptcha/api/siteverify", undefined, {
+    params: {
+      secret,
+      response: token,
+    },
+  });
+
+  console.log("reCAPTCHA siteverify: ", response.data);
+
+  const success = response.data.success;
+  const challengeTimestamp = response.data.challenge_ts;
+  const hostname = response.data.hostname;
+  const errorCodes = response.data["error-codes"];
+
+  res.json({
+    success,
+    challengeTimestamp,
+    hostname,
+    errorCodes,
+  });
+};
+
+const verifyRecaptchaToken = async (args: { secret: string; token: string }) => {
+  const { secret, token } = args;
+  if (!token || !secret) {
+    throw new Error("reCAPTCHA secret or token is invalid");
+  }
+
+  const response = await axios.post("https://www.google.com/recaptcha/api/siteverify", undefined, {
+    params: {
+      secret,
+      response: token,
+    },
+  });
+
+  console.log("reCAPTCHA siteverify result: ", response.data);
+
+  if (!response.data.success) {
+    throw new Error("reCAPTCHA token is invalid");
+  }
+
+  return response.data;
+};
+
+const getSecretByRecaptchaVersion = (version: string) => {
+  switch (version) {
+    case "V2_CHECKBOX":
+      return "6LdH6-IiAAAAAB9ENqqRh8TMMwXVG5BTqydTjZoh";
+    case "V2_INVISIBLE":
+      return "6LddDOMiAAAAAIQybpd0_XUpvSI6RBn8lDTIsKMg";
+    case "V3":
+      return "6LcmreIiAAAAACTFMqenXmeoH_VQ2CA4Xitd0d6Q";
+    default:
+      throw new Error("reCAPTCHA secret not found");
+  }
+};
+
+const validateUsername = (username: string) => {
+  if (username.length < 2) {
+    throw new Error("Invalid username");
+  }
+};
+
+app.post("/api/user/registration", async (req, res) => {
+  const recaptchaVersion = _.trim(_.get(req.body, "recaptchaVersion"));
+  const username = _.trim(_.get(req.body, "username"));
+  const token = _.trim(_.get(req.body, "recaptchaToken"));
+
+  try {
+    validateUsername(username);
+    const secret = getSecretByRecaptchaVersion(recaptchaVersion);
+    await verifyRecaptchaToken({ token, secret });
+    res.status(201).json({
+      result: "SUCCESS",
+      username,
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(400).json({
+      result: "FAIL",
+      error: _.get(e, "message") ? _.get(e, "message") : JSON.stringify(e),
+    });
+  }
+});
+
+app.post("/api/recaptcha/verify/v3", async (req, res) => {
+  await verifyWithSecret("6LcmreIiAAAAACTFMqenXmeoH_VQ2CA4Xitd0d6Q")(req, res);
+});
+
+app.post("/api/recaptcha/verify/v2/checkbox", async (req, res) => {
+  await verifyWithSecret("6LdH6-IiAAAAAB9ENqqRh8TMMwXVG5BTqydTjZoh")(req, res);
+});
+
+app.post("/api/recaptcha/verify/v2/invisible", async (req, res) => {
+  await verifyWithSecret("6LddDOMiAAAAAIQybpd0_XUpvSI6RBn8lDTIsKMg")(req, res);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- frontend-v2/src/FormWithCheckbox.tsx
+++ frontend-v2/src/FormWithCheckbox.tsx
@@ -1,62 +1,69 @@
 import React from "react";
 import axios from "axios";
 import ReCAPTCHA from "react-google-recaptcha";
+import trim from "lodash/trim";
+
+const SITE_KEY = trim(process.env.REACT_APP_RECAPTCHA_KEY);
 
 const FormWithCheckbox: React.FC = () => {
-  const [recaptchaValue, setRecaptchaValue] = React.useState<any>(null);
-  const [recaptchaResult, setRecaptchaResult] = React.useState<any>(null);
+  const [username, setUsername] = React.useState("");
+  const [recaptchaToken, setRecaptchaToken] = React.useState<string | null>(null);
+  const [registrationResult, setRegistrationResult] = React.useState("");
+  const [errorMessage, setErrorMessage] = React.useState("");
 
-  const handleRecaptchaChange = React.useCallback((value: any) => {
+  const handleRecaptchaChange = React.useCallback((value: string | null) => {
     console.log("FormWithCheckbox::handleRecaptchaChange > value: ", value);
-    setRecaptchaValue(value);
+    setRecaptchaToken(value);
+  }, []);
+
+  const handleUsernameChange = React.useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = trim(event.target.value);
+    setUsername(value);
   }, []);
 
   const handleClickRegister = React.useCallback(async () => {
-    if (!recaptchaValue) {
-      alert("click recaptcha checkbox!");
+    setErrorMessage("");
+
+    if (!recaptchaToken) {
+      alert("Please click reCAPTCHA checkbox!");
       return;
     }
 
-    const token = await recaptchaValue;
-    console.log("FormWithCheckbox::handleClickRegister > token: ", token);
-
-    const recaptchaResponse = await axios.post(
-      "/api/recaptcha/verify/v2/checkbox",
-      {
-        token,
-      }
-    );
-    const recaptchaResult = recaptchaResponse.data;
-    console.log(
-      "FormWithCheckbox::handleClickRegister > recaptchaResult: ",
-      recaptchaResult
-    );
-
-    setRecaptchaResult(recaptchaResult);
-  }, [recaptchaValue]);
+    try {
+      const response = await axios.post("/api/user/registration", {
+        username,
+        recaptchaToken,
+        recaptchaVersion: "V2_CHECKBOX",
+      });
+      const { result } = response.data;
+      console.log("FormWithCheckbox::handleClickRegister > result: ", result);
+      setRegistrationResult(trim(result));
+      alert("Register success");
+    } catch (e: any) {
+      const response = e.response;
+      const { result, error } = response.data;
+      setRegistrationResult(trim(result));
+      setErrorMessage(trim(error));
+    }
+  }, [recaptchaToken, username]);
 
   return (
-    <div className="Form" data-testid="FormWithCheckbox">
+    <form className="Form" data-testid="FormWithCheckbox" data-registration-result={registrationResult}>
       <div>
         <label>
           <span>username: </span>
-          <input type="text" />
+          <input type="text" name="username" value={username} onChange={handleUsernameChange} />
         </label>
       </div>
 
-      <ReCAPTCHA
-        sitekey="6LdH6-IiAAAAAISJqrkoyhUjcZRtzWqLR0mTARrz"
-        onChange={handleRecaptchaChange}
-      />
+      <ReCAPTCHA sitekey={SITE_KEY} onChange={handleRecaptchaChange} />
 
-      <button onClick={handleClickRegister}>register</button>
+      <button type="button" onClick={handleClickRegister}>
+        register
+      </button>
 
-      {recaptchaResult && (
-        <div data-testid="RecaptchaResult">
-          {recaptchaResult.success ? "SUCCESS" : "FAIL"}
-        </div>
-      )}
-    </div>
+      {errorMessage && <div className="Error">{errorMessage}</div>}
+    </form>
   );
 };
 
